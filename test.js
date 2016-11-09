9845b3a09f1337de2ca0dd422834e8ab340f9270
严俊东 <1172482914@qq.com>
2016-11-09 20:29:37 +0800

    modify gitignore

--- test.js
+++ test.js
@@ -2,16 +2,16 @@
  * Created by qingyun on 16/9/25.
  */
 
-var express = require('express')
-var morgan = require('morgan')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser'); //post请求体的解析
+var app = express();
 
-var app = express()
+app.use(bodyParser.json()); //解析请求体的json
+app.use(bodyParser.urlencoded({extended: false})); //解决请求体的编码问题
 
-app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, 'public'))); //配置静态文件路径
 
-app.get('/', function (req, res) {
-    res.send('hello, world!')
-});
-app.listen(9999,function () {
+app.listen(9999, function () {
     console.log("http://localhost:9999");
 });
