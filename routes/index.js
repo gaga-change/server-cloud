6392b99eef2289360eeb665769edb8836043276a
严俊东 <1172482914@qq.com>
2016-12-17 13:01:14 +0800

    调整了下结构

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('book/index', {title: 'Express'});
-});
-
-router.post('/test', function (req, res, next) {
-    res.send('gaga');
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
