9b4ff0ff60a30eaf45ea918ed77b23a34a998414
严俊东 <1172482914@qq.com>
2016-12-17 14:32:41 +0800

    下拉菜单动画

--- public/blog/js/main.js
+++ public/blog/js/main.js
@@ -1,31 +1,37 @@
 /**
  * Created by dong on 2016/12/16.
  */
-var app = angular.module('Blog', []);
+var app = angular.module('Blog', ["ngAnimate"]);
 app.controller("MainCtrl", ['$scope', "$http", function ($scope, $http) {
+    $scope.ifShowSonMenu = false;
     $scope.showMenu = showMenu;
     $scope.hideMenu = hideMenu;
     $scope.showSonMenu = showSonMenu;
     $http({method: "get", url: "/data/type"}).success(function (data) {
         $scope.menuList = data["list"];
-    })
-}]);
-/*点击显示子菜单*/
-function showSonMenu() {
-    console.log("fds");
-    var sm = $("#sonMenu");
-    var c = $("#caret");
+    });
 
-    if (sm.hasClass("show")) {
-        sm.removeClass("show");
-    } else {
-        sm.addClass("show");
+    /*点击显示子菜单*/
+    function showSonMenu() {
+        console.log("fds");
+        if( $scope.ifShowSonMenu) {
+            $scope.ifShowSonMenu = false;
+        }else {
+            $scope.ifShowSonMenu = true;
+        }
+        var c = $("#caret");
+        if (c.hasClass("up")) {
+            c.removeClass("up");
+        } else {
+            c.addClass("up");
+        }
     }
-    if (c.hasClass("up")) {
-        c.removeClass("up");
-    } else {
-        c.addClass("up");
-    }
+}]);
+/*给“文档分类给具体的值”*/
+setMyClassHeight();
+function setMyClassHeight() {
+    var m = $("#myClass");
+    console.log(m.height());
 }
 
 /*显示侧边栏*/
