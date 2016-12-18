85186b10c79399f12877de4776002b559a6972f9
严俊东 <1172482914@qq.com>
2016-12-18 16:14:52 +0800

    后台创建假数据

--- routes/data.js
+++ routes/data.js
@@ -5,9 +5,12 @@ var express = require('express');
 var path = require("path");
 var router = express.Router();
 
-/* GET home page. */
+/*获取类型集合*/
 router.get('/type', function (req, res, next) {
     res.sendFile(path.dirname(__dirname) + "/data/type.json");
 });
-
+/*获取文档集合*/
+router.get('/document', function (req, res, next) {
+    res.sendFile(path.dirname(__dirname) + "/data/document.json");
+});
 module.exports = router;
